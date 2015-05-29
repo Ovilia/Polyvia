702bc335e86cf1484674d7a9a5e942b8c0e33101
Ovilia <zwl.sjtu@gmail.com>
2015-05-29 14:07:51 +0800

    upload image path displayed

--- src/js/app.js
+++ src/js/app.js
@@ -23,11 +23,26 @@ define(function(require, exports, module) {
     // dat.gui
     require('gui');
     var GuiConfig = function() {
+        this['Image Path'] = '18.jpg';
+
+        this['Upload Image'] = function() {
+            var input = document.getElementById('img-path');
+            input.addEventListener('change', function() {
+                var file = input.files[0];
+                config['Image Path'] = file.name;
+                // Iterate over all controllers
+                for (var i in gui.__controllers) {
+                    gui.__controllers[i].updateDisplay();
+                }
+            });
+            input.click();
+        };
+
         this['Vertex Cnt'] = 1000;
 
         this['Edge Weight'] = 0.8;
 
-        this['Render Again'] = function() {
+        this['Apply'] = function() {
             polyvia.set({
                 vertexCnt: this['Vertex Cnt'],
                 edgeWeight: 41 - this['Edges Weight'] * 40
@@ -37,7 +52,9 @@ define(function(require, exports, module) {
     };
     var config = new GuiConfig();
     var gui = new dat.GUI();
+    gui.add(config, 'Image Path', config['Image Path']);
+    gui.add(config, 'Upload Image');
     gui.add(config, 'Vertex Cnt', 100, 5000).step(100);
     gui.add(config, 'Edge Weight', 0, 1).step(0.05);
-    gui.add(config, 'Render Again');
+    gui.add(config, 'Apply');
 });
